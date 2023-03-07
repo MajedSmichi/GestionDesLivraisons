28feda05357841717b128f64c46c161907776b85
Majed Smichi <smichimajed@gmail.com>
2023-03-07 18:01:18 +0100

    get user list customer in adminDashboard

--- Frontend/src/views/dashboard/app/user-list.js
+++ Frontend/src/views/dashboard/app/user-list.js
@@ -1,119 +1,42 @@
-import React from 'react'
-import {Row,Col,Image} from 'react-bootstrap'
+import React, { useEffect, useState } from 'react'
+import {Row,Col,Image, Button} from 'react-bootstrap'
 import {Link} from 'react-router-dom'
 import Card from '../../../components/Card'
 
 // img
 import shap1 from '../../../assets/images/shapes/01.png'
-import shap2 from '../../../assets/images/shapes/02.png'
-import shap3 from '../../../assets/images/shapes/03.png'
-import shap4 from '../../../assets/images/shapes/04.png'
-import shap5 from '../../../assets/images/shapes/05.png'
-import shap6 from '../../../assets/images/shapes/06.png'
+import axios from 'axios'
+import { apiUrl } from '../../../Constants'
+import { AiOutlineDelete } from 'react-icons/ai'
 
-const userlist = [
-   {
-      img: `${shap1}`,
-      name: 'Anna Sthesia',
-      phone: '(760) 756 7568',
-      email: 'annasthesia@gmail.com',
-      country: 'USA',
-      status: 'Active',
-      company: 'Acme Corporation',
-      joindate: '2019/12/01',
-      color: 'bg-primary'
-   },
-   {
-      img: `${shap2}`,
-      name: 'Brock Lee',
-      phone: '+62 5689 458 658',
-      email: 'brocklee@gmail.com',
-      country: 'Indonesia',
-      status: 'Active',
-      company: 'Soylent Corp',
-      joindate: '2019/12/01',
-      color: 'bg-primary'
-   },
-   {
-      img: `${shap3}`,
-      name: 'Dan Druff',
-      phone: '+55 6523 456 856',
-      email: 'dandruff@gmail.com',
-      country: 'Brazil',
-      status: 'Pending',
-      company: 'Acme Corporation',
-      joindate: '2019/12/01',
-      color: 'bg-warning'
-   }, 
-   {
-      img: `${shap4}`,
-      name: 'Hans Olo',
-      phone: '+91 2586 253 125',
-      email: 'hansolo@gmail.com',
-      country: 'India',
-      status: 'Inactive',
-      company: 'Vehement Capital',
-      joindate: '2019/12/01', 
-      color: 'bg-danger'
-   },
-   {
-      img: `${shap5}`,
-      name: 'Lynn Guini',
-      phone: '+27 2563 456 589',
-      email: 'lynnguini@gmail.com',
-      country: 'Africa',
-      status: 'Active',
-      company: 'Massive Dynamic',
-      joindate: '2019/12/01',
-      color: 'bg-primary'
-   },
-   {
-      img: `${shap6}`,
-      name: 'Eric Shun',
-      phone: '+55 25685 256 589',
-      email: 'ericshun@gmail.com',
-      country: 'Brazil',
-      status: 'Pending',
-      company: 'Globex Corporation',
-      joindate: '2019/12/01',
-      color: 'bg-warning'
-   },
-   {
-      img: `${shap3}`,
-      name: 'aaronottix',
-      phone: '(760) 756 7568',
-      email: 'budwiser@ymail.com',
-      country: 'USA',
-      status: 'Hold',
-      company: 'Acme Corporation',
-      joindate: '2019/12/01',
-      color: 'bg-info'
-   },
-   {
-      img: `${shap5}`,
-      name: 'Marge Arita',
-      phone: '+27 5625 456 589',
-      email: 'margearita@gmail.com',
-      country: 'Africa',
-      status: 'Complite',
-      company: 'Vehement Capital',
-      joindate: '2019/12/01',
-      color: 'bg-success'
-   },
-   {
-      img: `${shap2}`,
-      name: 'Bill Dabear',
-      phone: '+55 2563 456 589',
-      email: 'billdabear@gmail.com',
-      country: 'Brazil',
-      status: 'Active',
-      company: 'Massive Dynamic',
-      joindate: '2019/12/01',
-      color: 'bg-primary'
-   }
-]
+
+
 
 const UserList =() =>{
+   const [userData, setUserData] = useState([]);
+    useEffect(()=>{
+      const getAllCustomersUser= async () => {
+         try {
+   
+           const response = await axios.get(`${apiUrl}/users/AllCustomersUsers`);
+           setUserData(response.data);
+         } catch (error) {
+           console.log(error);
+         }
+       };
+       getAllCustomersUser();
+    },[]);
+
+    const deleteuser=async(userData)=>{
+      try {
+         const res = await axios.delete(`${apiUrl}/users/${id}`);
+         // Remove the deleted user from the state
+         setUserData(userData.filter(user => user._id !== id));
+       } catch (error) {
+         console.log(error);
+       }
+    };
+   
   return(
      <>
        <div>
@@ -131,28 +54,24 @@ const UserList =() =>{
                            <thead>
                               <tr className="ligth">
                                  <th>Profile</th>
-                                 <th>Name</th>
+                                 <th>FirstName</th>
+                                 <th>LastName</th>
                                  <th>Contact</th>
                                  <th>Email</th>
-                                 <th>Country</th>
-                                 <th>Status</th>
-                                 <th>Company</th>
-                                 <th>Join Date</th>
+                                 <th>Adresse</th>
                                  <th min-width= "100px">Action</th>
                               </tr>
                            </thead>
-                           <tbody>
+                           <tbody >
                            { 
-                              userlist.map((item,idx) => (
+                              userData.map((item,idx) => (
                               <tr key={idx}>
                                  <td className="text-center"><Image className="bg-soft-primary rounded img-fluid avatar-40 me-3" src={item.img} alt="profile"/></td>
-                                 <td>{item.name}</td>
+                                 <td>{item.firstName}</td>
+                                 <td>{item.lastName}</td>
                                  <td>{item.phone}</td>
                                  <td>{item.email}</td>
-                                 <td>{item.country}</td>
-                                 <td><span className={`badge ${item.color}`}>{item.status}</span></td>
-                                 <td>{item.company}</td>
-                                 <td>{item.joindate}</td>
+                                 <td>{item.adresse}</td>
                                  <td>
                                     <div className="flex align-items-center list-user-action">
                                        <Link className="btn btn-sm btn-icon btn-success" data-toggle="tooltip" data-placement="top" title="" data-original-title="Add" to="#">
@@ -174,15 +93,10 @@ const UserList =() =>{
                                              </svg>
                                           </span>
                                        </Link>{' '}
-                                       <Link className="btn btn-sm btn-icon btn-danger" data-toggle="tooltip" data-placement="top" title="" data-original-title="Delete" to="#">
-                                          <span className="btn-inner">
-                                             <svg width="20" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" stroke="currentColor">
-                                                <path d="M19.3248 9.46826C19.3248 9.46826 18.7818 16.2033 18.4668 19.0403C18.3168 20.3953 17.4798 21.1893 16.1088 21.2143C13.4998 21.2613 10.8878 21.2643 8.27979 21.2093C6.96079 21.1823 6.13779 20.3783 5.99079 19.0473C5.67379 16.1853 5.13379 9.46826 5.13379 9.46826" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"></path>
-                                                <path d="M20.708 6.23975H3.75" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"></path>
-                                                <path d="M17.4406 6.23973C16.6556 6.23973 15.9796 5.68473 15.8256 4.91573L15.5826 3.69973C15.4326 3.13873 14.9246 2.75073 14.3456 2.75073H10.1126C9.53358 2.75073 9.02558 3.13873 8.87558 3.69973L8.63258 4.91573C8.47858 5.68473 7.80258 6.23973 7.01758 6.23973" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"></path>
-                                             </svg>
-                                          </span>
-                                       </Link>{' '}
+                                          <button className="btn-inner btn-sm btn-icon btn btn-danger" onClick={deleteuser}>
+                                            <AiOutlineDelete/>
+                                          </button>
+                                       
                                     </div>
                                  </td>
                               </tr>))}
