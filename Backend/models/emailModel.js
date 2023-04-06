e44250447b95ef4e5effdc77ea2ed00e16bf3954
Majed Smichi <smichimajed@gmail.com>
2023-04-06 23:13:44 +0100

    "add general package.json"

--- Backend/models/emailModel.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require("mongoose");
-const emailSchema=mongoose.Schema({
-    data:String,
-    receiver:{type:String,default:"0"},
-    createdAt: { type: Date, default: Date.now },
-});
-module.exports = mongoose.model("email",emailSchema);
